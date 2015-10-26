4a36ca409c845f300ead30512ffda74cd175a69a
Sang Kim <slopsang1@gmail.com>
2015-10-26 10:16:20 -0700

    first tryout

--- js/main.js
+++ js/main.js
@@ -2,3 +2,22 @@
 
 /* Use clever selections and event handlers to assign the described interactivity */
 
+var input = null;
+var output= null;
+
+$("#change-content").click(function(){
+    input = $("#selector").val();
+    output = $("#newContent").val();
+    $(input).text(output);
+});
+
+$("#add-at-end").click(function(){
+    // var input = $("#selector").val();
+    // var output = $("#newContent").val();
+    $(input).append(output);
+})
+
+$("#move-after").click(function() {
+    $()
+})
+
